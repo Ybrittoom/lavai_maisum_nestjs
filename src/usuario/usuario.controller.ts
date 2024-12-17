31cfe692421daf22d893a9108d1a3b78b1760453
Yago Emanuel <yagoemanuel@gmail.com>
2024-12-17 13:32:57 -0300

    ...

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
+
+@Controller('/usuarios')
+export class UsuarioController {
+  private usuarioRepository = new UsuarioRepository();
+
+  //metodo post
+  @Post()
+  async criaUsuario(@Body() dadosUsuarios) {
+    this.usuarioRepository.salvar(dadosUsuarios);
+    return dadosUsuarios;
+  }
+
+  //metodo get
+  @Get()
+  async listUsuarios() {
+    return this.usuarioRepository.listar()
+  }
+}
